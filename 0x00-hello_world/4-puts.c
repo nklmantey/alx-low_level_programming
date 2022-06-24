de935a07a1a12960a97c9ba5087e9c2ef73ac085
Nana Kofi Larbi Mantey <nklmantey@st.ug.edu.gh>
2022-06-24 20:23:49 +0000

    print string with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
-int main(void){
+/**
+ * main - Entry point
+ *   
+ * Return: Always 0 (Success)
+ */
 
-	puts("Programming is like building a multilingual puzzle");
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
