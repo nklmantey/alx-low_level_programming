ec861ccae9b6c5125daca3d6474caa72ed84f9bb
Nana Kofi Larbi Mantey <nklmantey@st.ug.edu.gh>
2022-07-01 14:00:42 +0000

    print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabets;
+
+	for (alphabets = 'a', alphabets <= 'z', alphabets++)
+	{
+		putchar(alphabets);
+		putchar(\n);
+	}
+}
