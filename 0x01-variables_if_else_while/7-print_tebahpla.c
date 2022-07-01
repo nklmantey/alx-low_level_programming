0803782372b5d0a44469ddc63ea1910d6eda3dfe
Nana Kofi Larbi Mantey <nklmantey@st.ug.edu.gh>
2022-07-01 16:45:58 +0000

    reversed alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph = 'z';
+
+	while (alph >= 'a')
+	{
+		putchar(alph);
+		alph--;
+	}
+	putchar('\n');
+	return (0);
+}
