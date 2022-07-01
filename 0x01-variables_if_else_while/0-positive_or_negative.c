f4a564eb32c64d100adbf8c8aab42018c4e3d7d0
Nana Kofi Larbi Mantey <nklmantey@st.ug.edu.gh>
2022-07-01 13:50:32 +0000

    determine if input is negative, positive or zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,26 +5,23 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)  
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	
+	n = rand() - RAND_MAX / 2;	
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	} else if (n < 0)
 	{
 		printf("%d is negative\n", n);
-	} else 
+	} else
 	{
 		printf("%d is zero\n", n);
 	}
-
 	return (0);
-	
 }	
