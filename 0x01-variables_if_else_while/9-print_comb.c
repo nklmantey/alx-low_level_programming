c1f84742f3a47c6ce4318b57d7fb5b1e01ab8ce2
Nana Kofi Larbi Mantey <nklmantey@st.ug.edu.gh>
2022-07-01 17:12:51 +0000

    0-9 spaced with commas

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,8 +9,10 @@ int main(void)
 	int num;
 
 	for (num = 0; num < 10; num++)
-		printf("%d, ", num);
-	
+		if (num == 9)
+			printf("%d", num);
+		else
+			printf("%d, ", num);
 	putchar('\n');
 	return (0);
 }
