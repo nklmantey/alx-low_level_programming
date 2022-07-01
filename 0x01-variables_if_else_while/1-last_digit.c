cda777f33cc5384740b112017cd92cf66a8c504e
Nana Kofi Larbi Mantey <nklmantey@st.ug.edu.gh>
2022-07-01 13:53:49 +0000

    last digit is greater or less than

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /*
- * main - ENtry point
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
@@ -18,7 +18,7 @@ int main(void)
 	else if ((n % 10) == 0)
 		printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	else
-		printf("Last digit of %d is %d and is less than 6 and not 0", n, n % 10);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 
-	return 0;
+	return (0);
 }
