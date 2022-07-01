3fd580d3b742e80b6465fa3abe872e476aa39499
Nana Kofi Larbi Mantey <nklmantey@st.ug.edu.gh>
2022-07-01 16:55:28 +0000

    print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	int num;
+	char alph = 'a';
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+	
+	while (alph < 'g')
+	{
+		putchar(alph);
+		alph++;
+	}
+	putchar ('\n');
+	return (0);
+}
