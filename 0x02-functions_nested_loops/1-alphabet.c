a3525fc1406c605c50554e73d14423385b70a3fb
Nana Kofi Larbi Mantey <nklmantey@st.ug.edu.gh>
2022-07-05 23:29:31 +0000

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char start;
+
+	for (start = 'a'; start <= 'z'; start++)
+	{
+		_putchar(start);
+	}
+	putchar('\n');
+	return(0);
+}
