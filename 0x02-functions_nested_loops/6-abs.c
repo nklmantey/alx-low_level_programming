64bf071cdeabb9a1bcc7350453febfef878cb230
Nana Kofi Larbi Mantey <nklmantey@st.ug.edu.gh>
2022-07-06 00:35:04 +0000

    print absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int _abs(int abs)
+{
+	if (abs >= 0)
+	{
+		return (abs);
+	}
+	else 
+	{
+		return (abs * -1);
+	}
+}
