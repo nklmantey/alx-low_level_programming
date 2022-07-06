4f90e0d3273d650016f80903bceb5af897cd8a8a
Nana Kofi Larbi Mantey <nklmantey@st.ug.edu.gh>
2022-07-06 01:57:30 +0000

    to and from 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - entry point
+ *
+ * @n: value
+ *
+ * Return: Always 0
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		for (n = n; n < 98; n++)
+		{
+			printf("%d, ", n);
+			printf("%d\n, ", 98);
+		}
+	}
+	else
+	{
+		for (n = n; n > 98; n--)
+		{
+			printf("%d, ", n);
+			printf("%d\n, ", 98);
+		}
+	}
+}
