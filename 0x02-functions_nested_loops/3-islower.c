31deb1857e135273bac89c1d482687d7ea2d1e90
Nana Kofi Larbi Mantey <nklmantey@st.ug.edu.gh>
2022-07-06 00:06:52 +0000

    return 1 or 0 if lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - entry point
+ *
+ * @C: ASCII code
+ *
+ * Return: 1 if lowercase and 0 if not
+ */
+int _islower(int c)
+{
+	if (c > 'a' && c < 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
