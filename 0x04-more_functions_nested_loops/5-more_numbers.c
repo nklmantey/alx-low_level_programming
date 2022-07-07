7e5dd6843adb4104aa352e3f2a9a4b5f635e7209
Nana Kofi Larbi Mantey <nklmantey@st.ug.edu.gh>
2022-07-07 21:38:20 +0000

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,11 +7,11 @@
 void more_numbers(void)
 {
 	int count;
-	char start;
+	int start;
 
 	for (count = 0; count < 10; count++)
 	{
-		for (start = 0; start <= 14; start++)
+		for (start = 0; start < 15; start++)
 		{
 			if (start > 9)
 			{
