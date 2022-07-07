7663bd34a6609ab9efe52a53afc6ba24adfeea27
Nana Kofi Larbi Mantey <nklmantey@st.ug.edu.gh>
2022-07-07 21:48:09 +0000

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ *
+ * @n: line
+ *
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	int a;
+
+	for (a = 0; a < n; a++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
