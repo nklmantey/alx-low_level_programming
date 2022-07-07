ada4cc5f1028bf531ee4755ee460bf6e0886a7fc
Nana Kofi Larbi Mantey <nklmantey@st.ug.edu.gh>
2022-07-07 12:35:48 +0000

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit
+ *
+ * @c: value to check
+ *
+ * Return: 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
