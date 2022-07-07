23c9bf79d7f270a90446e25209543046473258e9
Nana Kofi Larbi Mantey <nklmantey@st.ug.edu.gh>
2022-07-07 10:00:05 +0000

    0x03-debugging task

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * main - tests function that prints if integer is positive or negative
+ * main - debugging
+ *
  * Return: 0
  */
 int main(void)
@@ -9,6 +10,5 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
-
 	return (0);
 }
