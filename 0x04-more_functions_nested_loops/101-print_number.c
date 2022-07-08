9a2cdaeb8ae23e982c61dfa1854a400be946bc18
Nana Kofi Larbi Mantey <nklmantey@st.ug.edu.gh>
2022-07-08 09:54:29 +0000

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * print_number - prints # using _putchar function
+ * @n: the integer to print
+ * Return: nothing
+ */
+void print_number(int n)
+{
+	int copy;
+	int nth;
+	int size = 1;
+	int ones = n % 10;
+
+	n /= 10;
+	copy = n;
+	if (ones < 0)
+	{
+		ones *= -1, copy *= -1, n *= -1;
+		_putchar('-');
+	}
+	if (copy > 0)
+	{
+		while (copy / 10 != 0)
+		{
+			copy /= 10, size *= 10;
+		}
+		while (size > 0)
+		{
+			nth = n / size;
+			_putchar('0' + nth);
+			n -= nth * size;
+			size /= 10;
+		}
+	}
+	_putchar('0' + ones);
+}
