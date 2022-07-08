2d3af6bc6535f44db47fac359817799f3648bbc7
Nana Kofi Larbi Mantey <nklmantey@st.ug.edu.gh>
2022-07-08 09:14:01 +0000

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_square - function to print a square
+ * @size: takes in the size of the square
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
