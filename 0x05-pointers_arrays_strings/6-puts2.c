f4904739b2575ecf26abcf9a8ed77c178a13610e
Nana Kofi Larbi Mantey <nklmantey@st.ug.edu.gh>
2022-07-11 18:25:28 +0000

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - print string
+ *
+ * @str: string
+ */
+void puts2(char *str)
+{
+	while (*str > 0)
+	{
+		if (*str % 2 == '\0')
+		{
+			_putchar(*str);
+		}
+		str++;
+	}
+	_putchar('\n');
+}
