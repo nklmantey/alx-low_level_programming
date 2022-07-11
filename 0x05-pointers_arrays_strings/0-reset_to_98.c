a361ad780b3487ce00dfdd93ee43a93538afd57f
Nana Kofi Larbi Mantey <nklmantey@st.ug.edu.gh>
2022-07-11 16:36:53 +0000

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * reset_to_98 - takes a pointer to an int as parameter and updates the value it points to to 98
+ * reset_to_98 - point to 98
  *
- * @*n: value
+ * @n: value
  *
  * Return: 0
  */
