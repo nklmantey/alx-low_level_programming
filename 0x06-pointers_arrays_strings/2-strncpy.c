8dc1f3afa7b6d41285e72e7481f47c498c409520
Nana Kofi Larbi Mantey <nklmantey@st.ug.edu.gh>
2022-07-13 20:46:10 +0000

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strncpy - copy number
+ * @dest: value
+ * @src: value
+ * @n: value
+ *
+ * Return: pointer
+ */
+char *_strncpy( char *dest, char *src, int n)
+{
+	int i = 0;
+	int s = 0;
+
+	while (src[i++])
+	{
+		s++;
+	}
+	for (i = 0; src[i] && i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (i = s; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
+}
