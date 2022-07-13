27c42377469984610bc6b350c67720ae42b12afb
Nana Kofi Larbi Mantey <nklmantey@st.ug.edu.gh>
2022-07-13 19:53:06 +0000

    initial commit

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef main
+#define main
+
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -11,3 +14,5 @@ char *rot13(char *a);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+
+#endif
