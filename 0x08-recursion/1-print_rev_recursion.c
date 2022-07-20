c8ba329d7f387626894fdafcb3029ea5fb795ee3
Nana Kofi Larbi Mantey <nklmantey@st.ug.edu.gh>
2022-07-20 10:40:06 +0000

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _print_rev_recursion
+ * _print_rev_recursion - reverse string
  *
  * @s: value
  *
@@ -8,10 +8,9 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		return;
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-	_putchar(*s);
-	_print_rev_recursion(s - 1);
 }
