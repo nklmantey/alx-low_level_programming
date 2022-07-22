352c493989c7ebba2c2c24e475c008754c265176
Nana Kofi Larbi Mantey <nklmantey@st.ug.edu.gh>
2022-07-22 17:24:49 +0000

    0x09

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -1,1 +1,21 @@
-char *_memcpy(char *dest, char *src, unsigned int n);
+#include "main.h"
+/**
+ * _memcpy - copy
+ * @dest: value
+ * @src: value
+ * @n: bytes
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+}
--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,1 +1,40 @@
-int _atoi(char *s);
+#include "main.h"
+/**
+ * is_numerical - digit check
+ * @n: number
+ * Return: 1
+ */
+
+int is_numerical(unsigned int n)
+{
+	return (n >= '0' && n <= '9');
+}
+
+/**
+ * _atoi - convert string
+ * @s: string
+ * Return: value
+ */
+int _atoi(char *s)
+{
+	unsigned int number, i;
+	int sign;
+
+	sign = 1;
+	number = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (is_numerical(s[i]))
+		{
+			number = (s[i] - 48) + number * 10;
+			if (s[i + 1] == ' ')
+				break;
+		}
+		else if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+	}
+	return (number * sign);
+}
