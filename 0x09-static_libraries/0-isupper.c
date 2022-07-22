50240e46a3898e706771d3d2aff2c9b511b3edc7
Nana Kofi Larbi Mantey <nklmantey@st.ug.edu.gh>
2022-07-22 15:40:07 +0000

    0x09

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - is upper
+ * @c: value
+ * Return: 1
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset - stuff
+ * @s: pointer
+ * @b: value
+ * @n: value
+ * Return: pointer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
