8218ec3094301f4dce35f3d6aef6bcbcf468fb4f
Nana Kofi Larbi Mantey <nklmantey@st.ug.edu.gh>
2022-07-25 11:18:38 +0000

    updated answer

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -22,8 +22,8 @@ char *argstostr(int ac, char **av)
 		size++;
 	}
 	size++;
-/** 
- * allocate memory for total number of chars and
+/**
+ * strDup - allocate memory for total number of chars and
  * new line for each word
  */
 	strDup = malloc(sizeof(char) * size);
