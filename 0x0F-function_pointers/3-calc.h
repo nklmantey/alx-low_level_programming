9c059e01497d20d4286ad28feca8a310b419d909
Nana Kofi Larbi Mantey <nklmantey@st.ug.edu.gh>
2022-08-03 19:43:29 +0000

    final changes

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,5 @@
-#ifndef OPERATIONS
-#define OPERATIONS
+#ifndef CALC_H
+#define CALC_H
 /**
  * struct op - Struct op
  *
@@ -10,7 +10,8 @@ typedef struct op
 {
 	char *op;
 	int (*f)(int a, int b);
-}	op_t;
+} op_t;
+
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
